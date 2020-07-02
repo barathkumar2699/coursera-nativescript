acc1341762afdd5aeb8003b06e723f58f70fb7f2
Barath <barath6.99@gmail.com>
2020-07-02 13:02:49 +0530

    NativeScript Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { HttpClientModule } from '@angular/common/http';
+import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MenuComponent } from './menu/menu.component';
+
+import { DishService } from './services/dish.service';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+
+import { baseURL } from './shared/baseurl';
+
+// Uncomment and add to NgModule imports if you need to use two-way binding
+// import { NativeScriptFormsModule } from "nativescript-angular/forms";
+
+// Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
+// import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        AppRoutingModule,
+        NativeScriptHttpClientModule,
+        HttpClientModule
+    ],
+    declarations: [
+        AppComponent,
+        MenuComponent
+    ],
+    providers: [
+    {provide: 'baseURL', useValue: baseURL},
+    DishService,
+    ProcessHTTPMsgService],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+/*
+Pass your application module to the bootstrapModule function located in main.ts to start your app
+*/
+export class AppModule { }
---
+++ src/app/shared/baseurl.ts
@@ -0,0 +1,3 @@
+export const baseURL = "http://192.168.43.100:3000/";
+
+//json-server --host 192.168.43.100 --port 3000 db.json --watch 
