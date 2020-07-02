ad7f130cf0257852fe2455bf51565f275444d3a4
Barath <barath6.99@gmail.com>
2020-07-02 17:59:04 +0530

    Side Drawer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { HttpClientModule } from '@angular/common/http';
 import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -27,7 +28,8 @@ import { baseURL } from './shared/baseurl';
         NativeScriptModule,
         AppRoutingModule,
         NativeScriptHttpClientModule,
-        HttpClientModule
+        HttpClientModule,
+        NativeScriptUISideDrawerModule
     ],
     declarations: [
         AppComponent,
